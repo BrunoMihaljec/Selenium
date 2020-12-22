7f164f502ccc2c4cff0edac6261436a2989c1388
BrunoMihaljec <75934875+BrunoMihaljec@users.noreply.github.com>
2020-12-22 23:57:48 +0100

    First Commit

---
+++ ZadaciSelenium/Selenium/PageObjects/ShoppingPageObjects.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using Selenium;
+using System;
+using Selenium.Web_Driver;
+using Selenium.Methods;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium.PageObjects
+{
+    static class ShoppingPageObjects
+    {
+        private static IWebDriver driver = WebDriver.driver;
+
+        public static IWebElement btnGoToProduct = WebDriver.driver.FindElement(By.XPath("//*[@id='center_column']/ul/li/div/div[1]/div/a[1]"));
+
+   
+
+        public static void SelectProduct()
+        {
+            try
+            {             
+
+                SeleniumSetMethod.ElementPresent(btnGoToProduct);
+                SeleniumSetMethod.ElementEnabled(btnGoToProduct);
+                SeleniumSetMethod.Clicks(btnGoToProduct);
+
+                string urlSelectedProduct = driver.Url;
+                SeleniumSetMethod.WaitForPageToLoad(driver, 50);
+                SeleniumGetMethods.PageLoaded(urlSelectedProduct, "product");
+                Console.WriteLine("Viewing product successful!");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Viewing product failed: {0}", e);
+            }
+        }
+
+
+        
+    }
+}
