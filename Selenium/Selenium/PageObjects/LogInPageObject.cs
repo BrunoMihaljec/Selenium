8e7006907568f56b81ccd2e953ccf80e246ce4c1
BrunoMihaljec <75934875+BrunoMihaljec@users.noreply.github.com>
2020-12-23 10:14:57 +0100

    First Commit

---
+++ Selenium/Selenium/PageObjects/LogInPageObject.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using Selenium;
+using System;
+using Selenium.Web_Driver;
+using Selenium.Methods;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Selenium.PageObjects
+{
+    static class LogInPageObject
+    {
+        private static IWebDriver driver = WebDriver.driver;
+
+        private static IWebElement txtEmailAdress = WebDriver.driver.FindElement(By.Name("email"));
+
+
+        public static IWebElement  txtPassword = WebDriver.driver.FindElement(By.Name("passwd"));
+
+
+        public static IWebElement btnSignIn = WebDriver.driver.FindElement(By.Name("SubmitLogin"));
+
+
+        public static void LogIn(string emailadress, string password)
+        {
+            try
+            {
+                SeleniumSetMethod.ElementPresent(txtEmailAdress);
+                SeleniumSetMethod.ElementEnabled(txtEmailAdress);
+                SeleniumSetMethod.ElementEmpty(txtEmailAdress);
+                txtEmailAdress.Clear();
+                SeleniumSetMethod.EnterText(txtEmailAdress, emailadress);
+
+                SeleniumSetMethod.ElementPresent(txtPassword);
+                SeleniumSetMethod.ElementEnabled(txtPassword);
+                SeleniumSetMethod.ElementEmpty(txtPassword);
+                txtPassword.Clear();
+                SeleniumSetMethod.EnterText(txtPassword, password);
+
+                SeleniumSetMethod.ElementPresent(btnSignIn);
+                SeleniumSetMethod.ElementEnabled(btnSignIn);
+                SeleniumSetMethod.Submits(btnSignIn);
+
+                string urlSearch = driver.Url;
+                SeleniumSetMethod.WaitForPageToLoad(driver, 35);
+                SeleniumGetMethods.PageLoaded(urlSearch, "account");
+                Console.WriteLine("Login successful!");
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("LogIn failed: {0}", e);
+            }
+        }
+
+    }
+}
---
+++ Selenium/Selenium/PageObjects/SearchPageObject.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using Selenium;
+using System;
+using Selenium.Web_Driver;
+using Selenium.Methods;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Selenium.PageObjects
+{
+    static class SearchPageObject
+    {
+        private static IWebDriver driver = WebDriver.driver;
+
+        public static IWebElement txtSearch = WebDriver.driver.FindElement(By.Name("search_query"));
+
+
+        public static IWebElement btnSearch = WebDriver.driver.FindElement(By.Name("submit_search"));
+
+
+
+        public static void EnterProduct(string search)
+        {
+            try
+            {
+                SeleniumSetMethod.ElementPresent(txtSearch);
+                SeleniumSetMethod.ElementEnabled(txtSearch);
+                SeleniumSetMethod.ElementEmpty(txtSearch);
+                SeleniumSetMethod.EnterText(txtSearch, search);
+
+                SeleniumSetMethod.ElementPresent(btnSearch);
+                SeleniumSetMethod.ElementEnabled(btnSearch);
+                SeleniumSetMethod.Submits(btnSearch);
+
+                string urlProducts = driver.Url;
+                SeleniumSetMethod.WaitForPageToLoad(driver, 50);
+                SeleniumGetMethods.PageLoaded(urlProducts, "shirt");
+                Console.WriteLine("Search successful!");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Search failed: {0}", e);
+            }
+        }      
+    }
+}
